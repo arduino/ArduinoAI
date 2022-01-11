aaa3a780868883b60829c4ad35895ac73b623a7b
marqdevx <p.marquinez@arduino.cc>
2022-01-11 13:00:30 +0100

    Update Sketch adding more sensors

--- NiclaSenseME-dashboard/NiclaSenseME/NiclaSenseME.ino
+++ NiclaSenseME-dashboard/NiclaSenseME/NiclaSenseME.ino
@@ -32,12 +32,17 @@ BLEService service(BLE_SENSE_UUID("0000"));
 BLEUnsignedIntCharacteristic versionCharacteristic(BLE_SENSE_UUID("1001"), BLERead);
 BLEFloatCharacteristic temperatureCharacteristic(BLE_SENSE_UUID("2001"), BLERead);
 BLEUnsignedIntCharacteristic humidityCharacteristic(BLE_SENSE_UUID("3001"), BLERead);
-BLEUnsignedIntCharacteristic pressureCharacteristic(BLE_SENSE_UUID("4001"), BLERead);
+BLEFloatCharacteristic pressureCharacteristic(BLE_SENSE_UUID("4001"), BLERead);
 
-BLECharacteristic accelerometerCharacteristic(BLE_SENSE_UUID("5001"), BLERead | BLENotify, 3 * sizeof(float)); // Array of 3 bytes, RGB
-BLECharacteristic gyroscopeCharacteristic(BLE_SENSE_UUID("6001"), BLERead | BLENotify, 3 * sizeof(int16_t));   // Array of 3 bytes, RGB
+BLECharacteristic accelerometerCharacteristic(BLE_SENSE_UUID("5001"), BLERead | BLENotify, 3 * sizeof(float));  // Array of 3x 2 Bytes, XY
+BLECharacteristic gyroscopeCharacteristic(BLE_SENSE_UUID("6001"), BLERead | BLENotify, 3 * sizeof(int16_t));    // Array of 3x 2 Bytes, XYZ
+BLECharacteristic quaternionCharacteristic(BLE_SENSE_UUID("7001"), BLERead | BLENotify, 4 * sizeof(float));     // Array of 4x 2 Bytes, XYZW
 
-BLECharacteristic rgbLedCharacteristic(BLE_SENSE_UUID("7001"), BLERead | BLEWrite, 3 * sizeof(byte)); // Array of 3 bytes, RGB
+BLECharacteristic rgbLedCharacteristic(BLE_SENSE_UUID("8001"), BLERead | BLEWrite, 3 * sizeof(byte)); // Array of 3 bytes, RGB
+
+BLEFloatCharacteristic bsecCharacteristic(BLE_SENSE_UUID("9001"), BLERead);
+BLEIntCharacteristic  co2Characteristic(BLE_SENSE_UUID("9002"), BLERead);
+BLEUnsignedIntCharacteristic gasCharacteristic(BLE_SENSE_UUID("9003"), BLERead); 
 
 // String to calculate the local and device name
 String name;
@@ -45,24 +50,31 @@ String name;
 Sensor temperature(SENSOR_ID_TEMP);
 Sensor humidity(SENSOR_ID_HUM);
 Sensor pressure(SENSOR_ID_BARO);
+Sensor gas(SENSOR_ID_GAS);
 SensorXYZ gyroscope(SENSOR_ID_GYRO);
 SensorXYZ accelerometer(SENSOR_ID_ACC);
+SensorQuaternion quaternion(SENSOR_ID_RV);
+SensorBSEC bsec(SENSOR_ID_BSEC);
 
 void setup(){
   Serial.begin(115200);
 
-  // while (!Serial);
-  Serial.println("Started");
+  Serial.println("Start");
 
   nicla::begin();
   nicla::leds.begin();
   nicla::leds.setColor(green);
+
+  //Sensors intialization
   BHY2.begin();
   temperature.begin();
   humidity.begin();
   pressure.begin();
   gyroscope.begin();
   accelerometer.begin();
+  quaternion.begin();
+  bsec.begin();
+  gas.begin();
 
   if (!BLE.begin()){
     Serial.println("Failled to initialized BLE!");
@@ -91,30 +103,38 @@ void setup(){
   BLE.setDeviceName(name.c_str());
   BLE.setAdvertisedService(service);
 
+  // Add all the previously defined Characteristics
   service.addCharacteristic(temperatureCharacteristic);
   service.addCharacteristic(humidityCharacteristic);
   service.addCharacteristic(pressureCharacteristic);
   service.addCharacteristic(versionCharacteristic);
   service.addCharacteristic(accelerometerCharacteristic);
   service.addCharacteristic(gyroscopeCharacteristic);
+  service.addCharacteristic(quaternionCharacteristic);
+  service.addCharacteristic(bsecCharacteristic);
+  service.addCharacteristic(co2Characteristic);
+  service.addCharacteristic(gasCharacteristic);
   service.addCharacteristic(rgbLedCharacteristic);
 
+  // Sensors event handlers
   temperatureCharacteristic.setEventHandler(BLERead, onTemperatureCharacteristicRead);
   humidityCharacteristic.setEventHandler(BLERead, onHumidityCharacteristicRead);
   pressureCharacteristic.setEventHandler(BLERead, onPressureCharacteristicRead);
+  bsecCharacteristic.setEventHandler(BLERead, onBsecCharacteristicRead);
+  co2Characteristic.setEventHandler(BLERead, onCo2CharacteristicRead);
+  gasCharacteristic.setEventHandler(BLERead, onGasCharacteristicRead);
 
   rgbLedCharacteristic.setEventHandler(BLEWritten, onRgbLedCharacteristicWrite);
 
   versionCharacteristic.setValue(VERSION);
 
   BLE.addService(service);
-
   BLE.advertise();
 }
 
 void loop(){
   while (BLE.connected()){
-    BHY2.update(100);
+    BHY2.update();
 
     if (gyroscopeCharacteristic.subscribed()){
       float x, y, z;
@@ -137,6 +157,18 @@ void loop(){
       float accelerometerValues[] = {x, y, z};
       accelerometerCharacteristic.writeValue(accelerometerValues, sizeof(accelerometerValues));
     }
+
+    if(quaternionCharacteristic.subscribed()){
+      float x, y, z, w;
+      x = quaternion.x();
+      y = quaternion.y();
+      z = quaternion.z();
+      w = quaternion.w();
+
+      float quaternionValues[] = {x,y,z,w};
+      quaternionCharacteristic.writeValue(quaternionValues, sizeof(quaternionValues));
+    }
+
   }
 }
 
@@ -151,8 +183,23 @@ void onHumidityCharacteristicRead(BLEDevice central, BLECharacteristic character
 }
 
 void onPressureCharacteristicRead(BLEDevice central, BLECharacteristic characteristic){
-  uint8_t pressureValue = pressure.value();
+  float pressureValue = pressure.value();
   pressureCharacteristic.writeValue(pressureValue);
+}
+
+void onBsecCharacteristicRead(BLEDevice central, BLECharacteristic characteristic){
+  float airQuality = float(bsec.iaq());
+  bsecCharacteristic.writeValue(airQuality);
+}
+
+void onCo2CharacteristicRead(BLEDevice central, BLECharacteristic characteristic){
+  uint32_t co2 = bsec.co2_eq();
+  co2Characteristic.writeValue(co2);
+}
+
+void onGasCharacteristicRead(BLEDevice central, BLECharacteristic characteristic){
+  unsigned int g = gas.value();
+  gasCharacteristic.writeValue(g);
 }
 
 void onRgbLedCharacteristicWrite(BLEDevice central, BLECharacteristic characteristic){
